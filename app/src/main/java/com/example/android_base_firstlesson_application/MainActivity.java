ed9bd765814020af755f5b02ef818de246b6dd88
Catrin <rude_box@mail.ru>
2021-05-29 23:49:39 +0300

    Lesson 1 task

---
+++ app/src/main/java/com/example/android_base_firstlesson_application/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.android_base_firstlesson_application;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setContentView(R.layout.additional_layout);
+    }
+
+}
